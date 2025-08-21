1af33059590d249279573ed0e61d4d7be701302c
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-08-21 20:29:49 -0300

    Feat: classe transacao criada

--- digital-bank-api/src/main/java/com/github/jeffmmartins/digital_bank_api/model/Transacao.java
+++ digital-bank-api/src/main/java/com/github/jeffmmartins/digital_bank_api/model/Transacao.java
@@ -9,4 +9,43 @@ public class Transacao {
     private Double valorDaTransacao;
     private TipoTransacao tipoDeTransacao;
     private LocalDate dataDaTransacao;
+
+    public Transacao(){
+
+    }
+
+    public Transacao(Double valorDaTransacao, TipoTransacao tipoDeTransacao, LocalDate dataDaTransacao) {
+        this.valorDaTransacao = valorDaTransacao;
+        this.tipoDeTransacao = tipoDeTransacao;
+        this.dataDaTransacao = dataDaTransacao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public Double getValorDaTransacao() {
+        return valorDaTransacao;
+    }
+
+    public void setValorDaTransacao(Double valorDaTransacao) {
+        this.valorDaTransacao = valorDaTransacao;
+    }
+
+    public TipoTransacao getTipoDeTransacao() {
+        return tipoDeTransacao;
+    }
+
+    public void setTipoDeTransacao(TipoTransacao tipoDeTransacao) {
+        this.tipoDeTransacao = tipoDeTransacao;
+    }
+
+    public LocalDate getDataDaTransacao() {
+        return dataDaTransacao;
+    }
+
+    public void setDataDaTransacao(LocalDate dataDaTransacao) {
+        this.dataDaTransacao = dataDaTransacao;
+    }
 }
