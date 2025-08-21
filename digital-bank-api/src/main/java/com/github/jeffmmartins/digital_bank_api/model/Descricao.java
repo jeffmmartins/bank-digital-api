7490168b6744aac60b4b4a8fdb88adec59c4c7db
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-08-21 10:57:30 -0300

    Feat: classe descricao criada

--- digital-bank-api/src/main/java/com/github/jeffmmartins/digital_bank_api/model/Descricao.java
+++ digital-bank-api/src/main/java/com/github/jeffmmartins/digital_bank_api/model/Descricao.java
@@ -1,5 +1,34 @@
 package com.github.jeffmmartins.digital_bank_api.model;
 
 public class Descricao {
+    private Long id;
+    private String titulo;
+    private String descricao;
 
+    public Descricao(){}
+
+    public Descricao(String titulo, String descricao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
