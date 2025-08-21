3b53ac938833b06b0742546493d0360487103bdb
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-08-21 10:50:26 -0300

    Feat: classe cliente criada

--- digital-bank-api/src/main/java/com/github/jeffmmartins/digital_bank_api/model/Cliente.java
+++ digital-bank-api/src/main/java/com/github/jeffmmartins/digital_bank_api/model/Cliente.java
@@ -1,4 +1,34 @@
 package com.github.jeffmmartins.digital_bank_api.model;
 
 public class Cliente {
+    private Long id;
+    private String nome;
+    private Integer cpf;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nome, Integer cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Integer cpf) {
+        this.cpf = cpf;
+    }
 }
--- digital-bank-api/src/main/java/com/github/jeffmmartins/digital_bank_api/model/Descricao.java
+++ digital-bank-api/src/main/java/com/github/jeffmmartins/digital_bank_api/model/Descricao.java
@@ -1,35 +1,5 @@
 package com.github.jeffmmartins.digital_bank_api.model;
 
 public class Descricao {
-    private Long id;
-    private String nome;
-    private Integer cpf;
 
-    public Descricao() {
-    }
-
-    public Descricao(String nome, Integer cpf) {
-        this.nome = nome;
-        this.cpf = cpf;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Integer getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(Integer cpf) {
-        this.cpf = cpf;
-    }
 }
