4265cfd33d58efef02111ad9fe92abbd9c0aecfc
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-09-01 07:51:53 -0300

    Feat: iniciando a classe TransacaoService

---
+++ digital-bank-api/src/main/java/com/github/jeffmmartins/digital_bank_api/service/TransacaoService.java
@@ -0,0 +1,16 @@
+package com.github.jeffmmartins.digital_bank_api.service;
+
+import com.github.jeffmmartins.digital_bank_api.repository.ClienteRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class TransacaoService {
+
+    private final ClienteRepository repository;
+
+
+
+
+}
