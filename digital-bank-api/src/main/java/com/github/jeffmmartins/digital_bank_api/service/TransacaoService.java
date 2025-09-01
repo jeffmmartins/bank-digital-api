1b216209182a57690e335d9359dccccf21b452a1
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-09-01 18:19:56 -0300

    Feat: metodo sacarDinheiro cirado

--- digital-bank-api/src/main/java/com/github/jeffmmartins/digital_bank_api/service/TransacaoService.java
+++ digital-bank-api/src/main/java/com/github/jeffmmartins/digital_bank_api/service/TransacaoService.java
@@ -3,21 +3,31 @@ package com.github.jeffmmartins.digital_bank_api.service;
 import com.github.jeffmmartins.digital_bank_api.model.Cliente;
 import com.github.jeffmmartins.digital_bank_api.model.Conta;
 import com.github.jeffmmartins.digital_bank_api.repository.ClienteRepository;
+import com.github.jeffmmartins.digital_bank_api.repository.ContaRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 @AllArgsConstructor
 public class TransacaoService {
 
-    private final ClienteRepository contaRepository;
+    private final ClienteRepository clienteRepository;
+    private final ContaRepository contaRepository;
 
     public Conta sacarDinheiro(Long idConta, double valorDoSaque){
-
         Conta conta = contaRepository.findById(idConta)
                 .orElseThrow(() -> new IllegalArgumentException("Conta não encontrada."));
 
+        if (conta.getSaldoDaConta() < valorDoSaque) {
+            throw new IllegalArgumentException("Saldo Insuficiente");
+        }
 
+        //Caso o saque seja efetivado, é atualizado o novo valor da conta
+        conta.setSaldoDaConta(conta.getSaldoDaConta()-valorDoSaque);
+
+        //Salva no banco de dados a conta com o novo valor do saldo.
+        return contaRepository.save(conta);
     }
-
 }
