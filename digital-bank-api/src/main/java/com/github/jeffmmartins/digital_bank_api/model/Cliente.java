2ad3838cb78af5162866f8bb47416462180fbf5f
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-09-18 18:18:06 -0300

    Refactor: alterações na classe cliente realizada

--- digital-bank-api/src/main/java/com/github/jeffmmartins/digital_bank_api/model/Cliente.java
+++ digital-bank-api/src/main/java/com/github/jeffmmartins/digital_bank_api/model/Cliente.java
@@ -1,44 +1,34 @@
 package com.github.jeffmmartins.digital_bank_api.model;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "tb_cliente")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE) // bloquear o setter para o id
     private Long id;
     private String nome;
+    @Column(unique = true,nullable = false)
+    @Setter(AccessLevel.NONE) // bloquear o setter para o cpf
     private String cpf;
     @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Conta> conta;
-
-    public Cliente() {
-    }
+    @Setter(AccessLevel.NONE) // bloquear o setter para o conta
+    private List<Conta> contas = new ArrayList<>();
 
     public Cliente(String nome, String cpf) {
         this.nome = nome;
-        this.cpf = cpf;
-    }
-
-    public Long getId() {
-        return id;
-    }
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
         this.cpf = cpf;
     }
 }
