c0c3bccee5db227b32119743c02fd60fd88c4a38
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-10-09 18:03:34 -0300

    Refactor: adicionando atributo id na TransacaoRequestDTO

--- digital-bank-api/src/main/java/com/github/jeffmmartins/digital_bank_api/dto/transacao/TransacaoRequestDTO.java
+++ digital-bank-api/src/main/java/com/github/jeffmmartins/digital_bank_api/dto/transacao/TransacaoRequestDTO.java
@@ -1,5 +1,6 @@
 package com.github.jeffmmartins.digital_bank_api.dto.transacao;
 
+import com.github.jeffmmartins.digital_bank_api.model.Transacao;
 import com.github.jeffmmartins.digital_bank_api.model.enums.TipoTransacao;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,6 +13,8 @@ import lombok.Setter;
 @NoArgsConstructor
 public class TransacaoRequestDTO {
 
+    private Long id;
     private Double valorDaTransacao;
     private TipoTransacao tipoDeTransacao;
+
 }
