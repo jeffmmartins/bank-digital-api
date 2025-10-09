22ebbebd8ab0389feeff10ce085cf306c5d5b753
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-10-09 17:46:08 -0300

    Refactor: anotações adicionadas

--- digital-bank-api/src/main/java/com/github/jeffmmartins/digital_bank_api/dto/conta/ContaRequestDTO.java
+++ digital-bank-api/src/main/java/com/github/jeffmmartins/digital_bank_api/dto/conta/ContaRequestDTO.java
@@ -1,13 +1,18 @@
 package com.github.jeffmmartins.digital_bank_api.dto.conta;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
-@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ContaRequestDTO {
 
     private String numeroDaConta;
     private String numeroDaAgencia;
     private Double saldoDaConta;
+
+
 }
--- digital-bank-api/src/main/java/com/github/jeffmmartins/digital_bank_api/dto/transacao/TransacaoRequestDTO.java
+++ digital-bank-api/src/main/java/com/github/jeffmmartins/digital_bank_api/dto/transacao/TransacaoRequestDTO.java
@@ -1,14 +1,17 @@
 package com.github.jeffmmartins.digital_bank_api.dto.transacao;
 
 import com.github.jeffmmartins.digital_bank_api.model.enums.TipoTransacao;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class TransacaoRequestDTO {
 
-    private Long id;
     private Double valorDaTransacao;
     private TipoTransacao tipoDeTransacao;
 }
