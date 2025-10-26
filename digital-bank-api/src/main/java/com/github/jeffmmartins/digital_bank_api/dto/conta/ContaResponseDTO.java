1b470bd37bfbb680218b3f57f07e5aba0f6557cd
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-10-26 08:52:02 -0300

    Refactor: refatorando a classe ContaResponseDTO

--- digital-bank-api/src/main/java/com/github/jeffmmartins/digital_bank_api/dto/conta/ContaResponseDTO.java
+++ digital-bank-api/src/main/java/com/github/jeffmmartins/digital_bank_api/dto/conta/ContaResponseDTO.java
@@ -3,11 +3,16 @@ package com.github.jeffmmartins.digital_bank_api.dto.conta;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal; // Importação corrigida
+
 @Getter
 @Setter
 public class ContaResponseDTO {
 
     private Long id;
-    private Double saldoDaConta;
+    private String numeroDaConta; // Adicionado
+    private String numeroDaAgencia; // Adicionado
+    private Long clienteId; // Adicionado
+    private BigDecimal saldoDaConta; // CORREÇÃO: Usando BigDecimal
 
 }
